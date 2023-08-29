251fbb3ddb4271fcba1acfa1170e3714f5b7d023
mariteixeiraa <marianatg.teixeira@gmail.com>
2023-08-29 12:17:32 -0300

    Initial commit

---
+++ src/br/com/alura/minhasmusicas/modelo/Musica.java
@@ -0,0 +1,42 @@
+package br.com.alura.minhasmusicas.modelo;
+
+public class Musica extends Audio {
+
+    private String almbum;
+    private String cantor;
+    private String genero;
+
+    public String getAlmbum() {
+        return almbum;
+    }
+
+    public void setAlmbum(String almbum) {
+        this.almbum = almbum;
+    }
+
+    public String getCantor() {
+        return cantor;
+    }
+
+    public void setCantor(String cantor) {
+        this.cantor = cantor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    @Override
+    public double getClassificacao() {
+        if (this.getTotalReproducoes() > 2000) {
+            return 10;
+        } else {
+            return 7;
+        }
+    }
+
+}
